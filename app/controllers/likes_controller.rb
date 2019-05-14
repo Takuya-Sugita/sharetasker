1a8d7691675d164a42f376a8c6d1495b3ca71d67
Takuya-Sugita <sukeofprlg@gmail.com>
2019-05-14 19:47:08 +0900

    change layout show of posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,12 +7,14 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
     @like.save
+    flash[:notice] = "TASKをチャレンジ！！"
     redirect_to("/posts/#{params[:post_id]}")
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     @like.destroy
+    flash[:notice] = "チャレンジをキャンセルしました"
     redirect_to("/posts/#{params[:post_id]}")
   end
 
