a49dc468e2819cebb3cd65d1787a2df31b708729
Takuya-Sugita <sukeofprlg@gmail.com>
2019-07-15 06:26:47 +0900

    add info to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,10 +93,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  host = 'website.com'
-  config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'sharetasker.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
