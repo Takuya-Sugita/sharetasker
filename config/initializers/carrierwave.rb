d77bbb139fbe15f9eca6b7e5e4bf59773b420831
Takuya-Sugita <sukeofprlg@gmail.com>
2019-07-30 17:58:19 +0900

    change strage to amazon s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAQ35TGLQJ6FSDEQPF',
+    aws_secret_access_key: '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',
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'sharetaskerbucket'
+  config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/sharetaskerbucket"
+  config.fog_public     = false
+  config.cache_storage = :fog
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
